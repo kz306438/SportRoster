1b34ebe63c7c9667d38c54d5bf62ace1f1067ed5
Кирилл <kz306438@gmail.com>
2025-03-02 23:00:09 +0300

    Add google test

---
+++ tests/PlayerTest.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "Player.h"
+
+namespace core {
+
+    TEST(PlayerTest, DefaultConstructor) {
+        Player player;
+        EXPECT_EQ(player.getName(), "");
+        EXPECT_EQ(player.getSurname(), "");
+        EXPECT_EQ(player.getAge(), 0);
+        EXPECT_EQ(player.getHeight(), 0);
+        EXPECT_EQ(player.getWeight(), 0);
+        EXPECT_EQ(player.getGameNumber(), 0);
+        EXPECT_EQ(player.getCountryName(), "");
+    }
+
+    TEST(PlayerTest, ParameterizedConstructor) {
+        Player player("John", "Doe", 25, 180, 75, 10, "USA");
+        EXPECT_EQ(player.getName(), "John");
+        EXPECT_EQ(player.getSurname(), "Doe");
+        EXPECT_EQ(player.getAge(), 25);
+        EXPECT_EQ(player.getHeight(), 180);
+        EXPECT_EQ(player.getWeight(), 75);
+        EXPECT_EQ(player.getGameNumber(), 10);
+        EXPECT_EQ(player.getCountryName(), "USA");
+    }
+
+} // namespace core
